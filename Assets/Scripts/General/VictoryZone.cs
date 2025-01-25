73af8f11c802b0a352f5028974488cd5ebf24ff2
Sergio Marchena <sergio.marchena@masiongames.com>
2025-01-25 20:56:58 +0100

    Fixed Victory Screen

--- Assets/Scripts/General/VictoryZone.cs
+++ Assets/Scripts/General/VictoryZone.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class VictoryZone : MonoBehaviour
 {
     public static Action OnVictory;
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         OnVictory?.Invoke();
     }
