bc5d280b74f879d912d94608990db232a107820c
Sergio Marchena <sergio.marchena@masiongames.com>
2025-01-26 11:20:55 +0100

    Adjusted life gain

--- Assets/Scripts/Enviroment/HealthPack.cs
+++ Assets/Scripts/Enviroment/HealthPack.cs
@@ -1,18 +1,19 @@
+
 using UnityEngine;
+
 
 public class HealthPack : MonoBehaviour
 {
-    [SerializeField] private int _health = 5; 
+	[SerializeField] private int _health = 10; 
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<PlayerController>() != null)
-        {
-            var player = other.GetComponent<PlayerController>();
-            if (player.Health == player.TotalHealth) return;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.TryGetComponent<PlayerController>(out var player))
+		{
+			if (player.Health == player.TotalHealth) return;
 
-            player.RestoreHealth(_health);
-            Destroy(gameObject);
-        }
-    }
+			player.RestoreHealth(_health);
+			Destroy(gameObject);
+		}
+	}
 }
