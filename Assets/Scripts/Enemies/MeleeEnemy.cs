10376fcee218011832a0674217ea9af1ac94b3d0
Sergio Marchena <sergio.marchena@masiongames.com>
2025-01-26 11:17:39 +0100

    Adjusted Damages

--- Assets/Scripts/Enemies/MeleeEnemy.cs
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -1,9 +1,13 @@
 
+using UnityEngine;
+
 using DG.Tweening;
 
 
 public class MeleeEnemy : Enemy
 {
+	[SerializeField] int _damage;
+
 	private void Awake()
 	{
 		_distanceToTarget = 1;
@@ -41,7 +45,7 @@ public class MeleeEnemy : Enemy
 		_meshRenderer.material.mainTexture = _attackSprite;
 		_state = EnemyState.Attacking;
 
-		PlayerController.OnHit(1);
+		PlayerController.OnHit(_damage);
 	}
 
 	protected override void Die()
