685ad2b144854e2e39b9a9b0b7e232f85b013804
Pablo Cáceres Gómez <pablo.caceres@masiongames.com>
2025-03-05 19:00:18 +0100

    LightningMap + UI Upgrades

--- Assets/Scripts/General/SpriteAnimation.cs
+++ Assets/Scripts/General/SpriteAnimation.cs
@@ -1,16 +1,20 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class SpriteAnimation : MonoBehaviour
 {
     [SerializeField] protected MeshRenderer _meshRenderer;
+    [SerializeField] protected Image _image;
 	[SerializeField] private Texture2D[] _sprites;
+	[SerializeField] private Sprite[] _imageSprites;
 	private int _spriteCounter = 0;
 
 	private void OnDestroy()
 	{
-		_meshRenderer.sharedMaterial.mainTexture = _sprites[0];
+		if (_meshRenderer != null) _meshRenderer.sharedMaterial.mainTexture = _sprites[0];
+		if (_image != null) _image.sprite = _imageSprites[0];
 	}
 
 	protected virtual void Update()
@@ -21,7 +25,9 @@ public class SpriteAnimation : MonoBehaviour
 	private void CheckSprite()
 	{
 		_spriteCounter++;
-		_meshRenderer.sharedMaterial.mainTexture = _sprites[_spriteCounter % 2];
+
+		if (_meshRenderer != null) _meshRenderer.sharedMaterial.mainTexture = _sprites[_spriteCounter % 2];
+		if (_image != null) _image.sprite = _imageSprites[_spriteCounter % 2];
 	}
 
 }
